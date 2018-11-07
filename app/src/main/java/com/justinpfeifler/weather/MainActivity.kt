ea641f6e96ea27e29e29b5ae032e929068019bc9
Justin Pfeifler <justin.pfeifler@revunit.com>
2018-11-07 14:55:22 -0600

    Add class for retrieving weather from RESTful Web Service

--- app/src/main/java/com/justinpfeifler/weather/MainActivity.kt
+++ app/src/main/java/com/justinpfeifler/weather/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.justinpfeifler.weather
 
 import android.content.Context
+import android.os.AsyncTask
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +13,13 @@ import android.widget.EditText
 import android.widget.ListView
 
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.net.HttpURLConnection
+import java.net.URL
+import java.net.URLEncoder
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,13 +43,87 @@ class MainActivity : AppCompatActivity() {
             if (url != null) {
                 dismissKeyboard(locationEditText)
                 val getLocationWeatherTask = GetWeatherTask()
-                getLocalWeatherTask.execute(url)
+                getLocationWeatherTask.execute(url)
             } else {
                 Snackbar.make(
                     findViewById(R.id.coordinator),
                     R.string.invalid_url,
                     Snackbar.LENGTH_LONG).show()
             }
+        }
+    }
+
+    private fun createURL(city: String) : URL? {
+        val key = getString(R.string.api_key)
+        val url = getString(R.string.web_service_url)
+
+        try {
+            val urlString = url + URLEncoder.encode(city, "UTF-8") + "&units=imperial&cnt=16&APPID=" + key
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        return null
+    }
+
+    private inner class GetWeatherTask : AsyncTask<URL, Void, JSONObject>() {
+        override fun doInBackground(vararg params: URL): JSONObject? {
+            var connection: HttpURLConnection? = null
+
+            try {
+                connection = params[0].openConnection() as HttpURLConnection
+                val response = connection.responseCode
+
+                if (response == HttpURLConnection.HTTP_OK) {
+                    val builder = StringBuilder()
+
+                    try {
+                        BufferedReader(InputStreamReader(connection.inputStream)).use {
+                            reader ->
+                            var line: String
+                            line = reader.readLine()
+                            builder.append(line)
+                            while (line.isNotEmpty()) {
+                                builder.append(line)
+                                line = reader.readLine()
+                            }
+                        }
+                    } catch (e: Exception) {
+                        Snackbar.make(findViewById(R.id.coordinatorLayout), R.string.read_error, Snackbar.LENGTH_LONG).show()
+                        e.printStackTrace()
+                    }
+
+                    return JSONObject(builder.toString())
+                } else {
+                    Snackbar.make(findViewById(R.id.coordinatorLayout), R.string.connect_error, Snackbar.LENGTH_LONG).show()
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            return null
+        }
+
+        override fun onPostExecute(weather: JSONObject?) {
+            weather?.let { convertJSONToList(it) }
+            weatherArrayAdapter!!.notifyDataSetChanged()
+            weatherListView!!.smoothScrollToPosition(0)
+        }
+    }
+
+    private fun convertJSONToList(forecast: JSONObject) {
+        var list = forecast.getJSONArray("list")
+        for (index in 0 until list.length()) {
+            val day = list.getJSONObject(index)
+            val temperatures = day.getJSONObject("temp")
+            val weather = day.getJSONArray("weather").getJSONObject(0)
+            weatherList.add( Weather(
+                day.getLong("dt"),
+                temperatures.getDouble("min"),
+                temperatures.getDouble("max"),
+                day.getDouble("humidity"),
+                weather.getString("description"),
+                weather.getString("icon")
+            ))
         }
     }
 
